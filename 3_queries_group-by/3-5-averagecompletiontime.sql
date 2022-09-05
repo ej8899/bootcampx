00aa3c08461eed19f1f40a11ead86318037b2cfd
ej8899 <ej8899@gmail.com>
2022-09-04 21:16:04 -0600

    add queries

--- 3_queries_group-by/3-5-averagecompletiontime.sql
+++ 3_queries_group-by/3-5-averagecompletiontime.sql
@@ -1,15 +1,9 @@
--- 3-5 - average completion time
-SELECT cohorts.name as cohort, count(*) as total_submissions
-FROM assignment_submissions
-JOIN students ON assignment_submissions.student_id = students.id
-JOIN cohorts ON cohorts.id = students.cohort_id
-GROUP by cohorts.name
-ORDER by count(*) DESC;
-
-
-SELECT students.name as student, AVG(duration) as average_assignment_duration
-FROM assignment_submissions
-JOIN students ON assignment_submissions.student_id = students.id
+-- 3-5 - average completion time - enrolled students only
+-- pay attention to what would be our primary table
+SELECT students.name AS student,
+AVG(assignment_submissions.duration) AS average_assignment_duration
+FROM students
+JOIN assignment_submissions ON students.id = assignment_submissions.student_id
+WHERE students.end_date IS NULL
 GROUP BY students.name
-ORDER BY average_assignment_duration DESC;
-WHERE students.end_date IS NULL;
+ORDER BY AVG(assignment_submissions.duration) DESC;
