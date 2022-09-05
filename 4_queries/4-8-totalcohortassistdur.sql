b5f0d69beb9603ba22052e1a501ee37d105aaa68
ej8899 <ej8899@gmail.com>
2022-09-04 21:04:56 -0600

    add queries

--- 4_queries/4-8-totalcohortassistdur.sql
+++ 4_queries/4-8-totalcohortassistdur.sql
@@ -9,5 +9,5 @@ from assistance_requests as ar
 join students as s on s.id = ar.student_id
 join cohorts as c on c.id = s.cohort_id
 group by c.name
-order by average_assistance_time desc
+order by total_duration asc
 ;
--- 4_queries/4-9-avgassistduration.sql
+++ 4_queries/4-9-avgassistduration.sql
@@ -1,12 +1,19 @@
 -- 4-9-avgassistduration.sql
 
+SELECT AVG (total_duration) as average_total_duration
+FROM (
+-- 4-8-totalcohortassistdur.sql
+
 select
-	--c.name,
+	c.name,
 	SUM(ar.completed_at - ar.started_at) AS total_duration
 from assistance_requests as ar
 
+
 join students as s on s.id = ar.student_id
 join cohorts as c on c.id = s.cohort_id
---group by c.name
+group by c.name
 order by total_duration asc
+
+) as total_duration
 ;
